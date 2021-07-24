d11411d226ac2f6f536e73536b85993bc7ce1bcf
atomixnmc <atomixnmc@gmail.com>
2021-07-24 18:46:23 -0400

    Rename vivoice to viai

--- src/data/myServices.js
+++ src/data/myServices.js
@@ -5,49 +5,49 @@ export default {
       description: "Next-gen ERP & Project Management",
       imageUrl: "/images/products/unibi.jpg",
       link: "https://unibi.surge.sh",
-      p: 1,
+      p: 1
     },
     {
       title: "UniQi",
       description: "Next-gen Education Platform",
       imageUrl: "/images/products/uniqi.jpg",
       link: "https://uniqi.surge.sh",
-      p: 1,
+      p: 1
     },
     {
       title: "UniFi",
       description: "Next-gen Finance Platform by Blockchain",
       imageUrl: "/images/products/unifi.jpg",
       link: "https://unifi.surge.sh",
-      p: 1,
+      p: 1
     },
     {
       title: "WebBuilder",
       description: "Web Builder",
       imageUrl: "/images/products/iWeb.jpg",
       link: "https://i2cwebbuilder.surge.sh",
-      p: 2,
+      p: 2
     },
     {
       title: "Tion",
       description: "Smart marketing and CRM",
       imageUrl: "/images/products/tion.jpg",
       link: "https://tion.surge.sh",
-      p: 2,
+      p: 2
     },
     {
-      title: "ViVoice",
+      title: "ViAI",
       description: "AI Assistant Platform",
-      imageUrl: "/images/products/vivoice.jpg",
-      link: "https://vivoice.surge.sh",
-      p: 2,
+      imageUrl: "/images/products/viai.jpg",
+      link: "https://viai.surge.sh",
+      p: 2
     },
     {
       title: "OSee",
       description: "Social Listening Platform",
       imageUrl: "/images/products/osee.jpg",
       link: "https://osee.surge.sh",
-      p: 2,
-    },
+      p: 2
+    }
   ]
-}
+};
